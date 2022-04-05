596068fbf49888d5920bb20d24c8dd257fa247d2
victorklippel <victor.souza@ilia.digital>
2022-04-05 14:52:19 -0300

    changes

--- App.js
+++ App.js
@@ -1,10 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
 
 export default function App() {
+  const [state, setState] = useState(2);
+
+  useEffect(() => {
+    setState(0);
+  }, []);
+
+  function plusCounter() {
+    setState((state) => state + 1);
+  }
+
+  function minusCounter() {
+    setState((state) => state - 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity
+          onPress={minusCounter}
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 15,
+            alignItems: "center",
+            borderColor: "#dcdcdc",
+            borderWidth: 1,
+          }}
+        >
+          <Text
+            style={{
+              textAlignVertical: "center",
+              fontSize: 18,
+              justifyContent: "center",
+            }}
+          >
+            -
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.textCenter}>{state}</Text>
+        <TouchableOpacity
+          onPress={plusCounter}
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 15,
+            alignItems: "center",
+            borderColor: "#dcdcdc",
+            borderWidth: 1,
+          }}
+        >
+          <Text
+            style={{
+              textAlignVertical: "center",
+              textAlign: "center",
+              fontSize: 18,
+              justifyContent: "center",
+            }}
+          >
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +80,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textCenter: {
+    paddingHorizontal: 20,
+    fontSize: 32,
   },
 });
